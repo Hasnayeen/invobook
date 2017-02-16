0f447994bc5496746f79b67c62f919d389a73379
Hasnayeen <searching.nehal@gmail.com>
2017-02-16 19:42:54 +0600

    Add seeder for admin user & headquarter office #5

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    const ADMIN  = 1;
+    const MEMBER = 2;
 }
---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'       => 'Admin',
+            'username'   => 'admin',
+            'email'      => 'admin@example.com',
+            'role'       => 1,
+            'active'     => 1,
+            'password'   => bcrypt('secret'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
