3ccc8393175fcd472a96fdf490d43f9775977b42
Hasnayeen <searching.nehal@gmail.com>
2017-02-16 19:42:54 +0600

    Add seeder for admin user & headquarter office #5

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdminUserSeeder::class);
+        $this->call(HeadquarterOfficeSeeder::class);
     }
 }
---
+++ database/seeds/HeadquarterOfficeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class HeadquarterOfficeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('offices')->insert([
+            'name'        => 'Headquarter',
+            'slug'        => 'headquarter',
+            'description' => 'Central office',
+            'created_at'  => Carbon::now(),
+            'updated_at'  => Carbon::now(),
+        ]);
+    }
+}
