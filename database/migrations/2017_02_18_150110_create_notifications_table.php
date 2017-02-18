489622f98d33db2c7595cf22fbe7c50dd7936aa2
Hasnayeen <searching.nehal@gmail.com>
2017-02-18 21:01:33 +0600

    Add laravel default notification table #1

--- database/migrations/2017_02_18_150110_create_notifications_table.php
+++ database/migrations/2017_02_18_150110_create_notifications_table.php
@@ -14,12 +14,11 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('text');
-            $table->string('url');
-            $table->boolean('seen')->default(false);
-            $table->string('notifiable_type')->comment('user, office, team or projects');
-            $table->integer('notifiable_id')->unsigned();
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
