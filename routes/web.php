71c4f921f6f36505e4ba65b897dc1ee06cf0a361
Hasnayeen <searching.nehal@gmail.com>
2017-02-18 22:02:33 +0600

    Add primary resource routes #4
    all resource routes added along with controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-Route::get('/', 'HomeController@index');
-
 Route::get('login', 'Auth\LoginController@showLoginForm');
 
 Route::post('login', 'Auth\LoginController@login');
@@ -15,3 +13,49 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/reset', 'Auth\ForgotPasswordController@reset');
 
 Route::get('password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm');
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/', 'HomeController@index');
+
+    Route::get('projects', 'ProjectController@index');
+
+    Route::get('projects/{project}', 'ProjectController@index');
+
+    Route::get('teams', 'TeamController@index');
+    
+    Route::get('teams/{team}', 'TeamController@index');
+
+    Route::get('offices', 'OfficeController@index');
+    
+    Route::get('offices/{office}', 'OfficeController@index');
+
+    Route::get('tasks', 'TaskController@index');
+    
+    Route::get('tasks/{task}', 'TaskController@index');
+
+    Route::get('discussions', 'DiscussionController@index');
+    
+    Route::get('discussions/{discussion}', 'DiscussionController@index');
+
+    Route::get('messages', 'MessageController@index');
+    
+    Route::get('messages/{message}', 'MessageController@index');
+
+    Route::get('events', 'EventController@index');
+    
+    Route::get('events/{event}', 'EventController@index');
+
+    Route::get('files', 'FileController@index');
+    
+    Route::get('files/{file}', 'FileController@index');
+
+    Route::get('users', 'UserController@index');
+    
+    Route::get('users/{user}', 'UserController@index');
+
+    Route::get('users/{user}/settings', 'UserController@index');
+
+    Route::get('settings', 'UserController@index');
+
+});
