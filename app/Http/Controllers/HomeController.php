c3eb3c0a25bd6cfb4d0203a817984e93b4b4193c
Nehal Hasnayeen <searching.nehal@gmail.com>
2017-02-18 11:37:15 +0600

    Add login page to redirect unauthenticated user (#24)
    separate route added for login and auth middleware added to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
