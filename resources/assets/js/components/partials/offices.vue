3ac90395a88e6e195ad26339e03985b43c7ebb46
Nehal Hasnayeen <searching.nehal@gmail.com>
2017-02-18 20:12:50 +0600

    Add vue vomponent for home page with dummy markup (#27)

---
+++ resources/assets/js/components/partials/offices.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="column is-one-third">
+        <div class="card justified">
+          <header class="card-header is-base-darker">
+            <a class="card-header-title">
+              Offices
+            </a>
+            <a class="card-header-icon">
+              <span class="icon">
+                <i class="fa fa-angle-down"></i>
+              </span>
+            </a>
+          </header>
+          <div class="card-content">
+            <!-- <div class=""> -->
+            <!-- <aside class="menu"> -->
+                
+                <div class="content">
+                    <a class="is-block has-text-centered" href="">Headquarter</a>
+                </div>
+            <!-- </aside> -->
+            <!-- </div> -->
+          </div>
+          <footer class="card-footer">
+            <a class="card-footer-item">Create a New Office</a>
+          </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
