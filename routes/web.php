69d1dd79d71e4f178eae1fecbdffcbd43d6e06b3
Nehal Hasnayeen <searching.nehal@gmail.com>
2017-02-18 11:37:15 +0600

    Add login page to redirect unauthenticated user (#24)
    separate route added for login and auth middleware added to home page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 Route::get('/', 'HomeController@index');
 
+Route::get('login', 'Auth\LoginController@showLoginForm');
+
 Route::post('login', 'Auth\LoginController@login');
 
 Route::post('logout', 'Auth\LoginController@logout');
