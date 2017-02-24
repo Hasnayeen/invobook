d0fa69de085c76a3c4fef111478ccc47133c75b7
Nehal Hasnayeen <searching.nehal@gmail.com>
2017-02-24 20:18:16 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -6,5 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,7 +23,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
