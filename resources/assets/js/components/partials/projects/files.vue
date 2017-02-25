e21fc32aaf5c5767f624c7815745033fc3fbcaf5
sourovroy <sourovfci@gmail.com>
2017-02-25 16:32:34 +0600

    Base vue component for single resource view added; Fixex #17 (#69)

---
+++ resources/assets/js/components/partials/projects/files.vue
@@ -0,0 +1,83 @@
+<template>
+<div class="column is-one-third">
+    <div class="card justified">
+        <header class="card-header">
+            <a href="#" class="card-header-title">
+                Files
+            </a>
+            <a class="card-header-icon">
+                <span class="icon">
+                    <i class="fa fa-angle-down"></i>
+                </span>
+            </a>
+        </header>
+        <div class="card-content">
+            <div class="content">
+
+                <article class="media taskboard-media">
+                    <a href="#">
+                    <figure class="media-left">
+                        <p class="image">
+                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>File Name</strong>
+                                <br>
+                                <span>Short Information</span>
+                            </p>
+                        </div>
+                    </div>
+                    </a>
+                </article>
+
+                <article class="media taskboard-media">
+                    <a href="#">
+                    <figure class="media-left">
+                        <p class="image">
+                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>File Name</strong>
+                                <br>
+                                <span>Short Information</span>
+                            </p>
+                        </div>
+                    </div>
+                    </a>
+                </article>
+
+                <article class="media taskboard-media">
+                    <a href="#">
+                    <figure class="media-left">
+                        <p class="image">
+                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>File Name</strong>
+                                <br>
+                                <span>Short Information</span>
+                            </p>
+                        </div>
+                    </div>
+                    </a>
+                </article>
+
+                <!--<p class="is-block has-text-centered">You have no project yet</p>-->
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
