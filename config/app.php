fc8cab3b271197082b19a1f9ad363b63cf5797c1
Aranya Sen <sen.aranya@gmail.com>
2017-02-25 11:44:24 +0530

    Remove third party package (laravel-ide-helper)

--- config/app.php
+++ config/app.php
@@ -175,9 +175,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-	    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
     ],
 
     /*
