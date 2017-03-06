9e41dcb99b7af9beb0b72765167f6d96a1f54a71
Hasnayeen <searching.nehal@gmail.com>
2017-03-06 15:41:56 +0600

    Update travis

--- tests/Browser/HomePageTest.php
+++ tests/Browser/HomePageTest.php
@@ -24,12 +24,12 @@ class HomePageTest extends DuskTestCase
     /** @test */
     public function seeLatestThreeProjectTeamAndOffice()
     {
-        $user = factory(User::class)->create();
+        $user     = factory(User::class)->create();
         $projects = factory(Project::class, 3)->create(['office_id' => null, 'team_id' => null]);
-        $teams = factory(Team::class, 3)->create(['office_id' => null]);
-        $offices = factory(Office::class, 3)->create();
+        $teams    = factory(Team::class, 3)->create(['office_id' => null]);
+        $offices  = factory(Office::class, 3)->create();
 
-        $this->browse(function ($browser) use ($user, $projects, $teams, $offices) {
+        $this->browse(function (Browser $browser) use ($user, $projects, $teams, $offices) {
             $browser->loginAs($user)
                     ->visit('/')
                     ->assertSeeIn('.columns div:nth-of-type(1) .content div:nth-of-type(1) a', $projects[0]['name'])
