d138e20af0823c5c25f5a1130033ffafce185489
Hasnayeen <searching.nehal@gmail.com>
2017-03-17 16:25:55 +0600

    Update dusk test case according to markup change

--- tests/Browser/ProjectListTest.php
+++ tests/Browser/ProjectListTest.php
@@ -18,10 +18,10 @@ class ProjectListTest extends DuskTestCase
         $this->browse(function (Browser $browser) use ($user, $projects) {
             $browser->loginAs($user)
                         ->visit('/projects')
-                        ->assertSeeIn('.projects-list ul li:nth-of-type(1) a', $projects[0]['name'])
-                        ->assertSeeIn('.projects-list ul li:nth-of-type(1) span', $projects[0]['description'])
-                        ->assertSeeIn('.projects-list ul li:nth-of-type(2) a', $projects[1]['name'])
-                        ->assertSeeIn('.projects-list ul li:nth-of-type(2) span', $projects[1]['description']);
+                        ->assertSeeIn('.column .card-content-item:nth-of-type(1) a', $projects[0]['name'])
+                        ->assertSeeIn('.column .card-content-item:nth-of-type(1) p', $projects[0]['description'])
+                        ->assertSeeIn('.column .card-content-item:nth-of-type(2) a', $projects[1]['name'])
+                        ->assertSeeIn('.column .card-content-item:nth-of-type(2) p', $projects[1]['description']);
         });
 
         $this->tearDown();
