42657d6df8d5e2529c3522b6ee49cbb0ae8bdc72
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-04-20 10:35:16 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -9,7 +9,7 @@ class UserAvatarController extends Controller
     public function store(Request $request)
     {
         auth()->user()->update([
-            'avatar' => $request->file('avatar')->storeAs('avatars', $request->user()->username . '.png', ['disk' => 'public']),
+            'avatar' => $request->file('avatar')->storeAs('avatars', $request->user()->username.'.png', ['disk' => 'public']),
         ]);
 
         return response()->json([], 204);
