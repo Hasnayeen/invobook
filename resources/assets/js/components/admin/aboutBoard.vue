6cbe0f8364a02b80b2af4fb57c6c566845b2b979
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-09-08 18:53:44 +0600

    Add option to check for version updates on about section #141 (#148)
    * Add option to check for version updates on about section #141

---
+++ resources/assets/js/components/admin/aboutBoard.vue
@@ -0,0 +1,92 @@
+<template>
+<div :class="{'hidden': (activeTab != 'about')}" class="w-full mb-8">
+  <div :class="{'hidden': !showUpdateModal}">
+      <div @click="closeUpdateModal" class="absolute pin opacity-75 bg-grey"></div>
+      <div class="fixed pin-x w-1/3 z-10 bg-grey-lighter mx-auto p-8 rounded">
+          <p class="py-2">
+            {{ message }}
+          </p>
+          <div class="flex flex-row justify-between pt-8 bg-grey-lighter rounded">
+              <button @click="closeUpdateModal" class="text-red-lighter hover:font-bold hover:text-red-light hover:border-red-light border-red-lighter border px-4 py-3 rounded">Cancel</button>
+              <button v-if="update_available" @click="updateProject" class="bg-teal-light text-white font-medium hover:bg-teal-dark py-3 px-4 rounded">Update</button>
+          </div>
+      </div>
+  </div>
+
+  <div class="container mx-4 md:mx-auto py-8 bg-white rounded shadow text-center text-grey-darker">
+    <div class="pb-4 text-3xl">
+      Goodwork
+    </div>
+    <div class="p-2">
+      Version: <span class="pl-2 text-xl text-grey-darkest">{{ version }}</span>
+      <button @click="checkForUpdates" class="no-underline p-4 m-4 mb-0 bg-white text-base text-teal rounded border border-grey-light">
+        Check for Updates
+        <div v-if="loading" class="inline">
+          <i class="fas fa-spinner fa-spin"></i>
+        </div>
+      </button>
+    </div>
+    <div class="p-2">
+      Last Updated: <span class="pl-2 text-xl text-grey-darkest">{{ lastUpdated }}</span>
+    </div>
+    <div class="p-2">
+      Author: <a href="https://hasnayeen.github.io/" class="pl-2 text-xl text-blue-light no-underline">Nehal Hasnayeen</a>
+    </div>
+    <div class="p-2">
+      Submit issue & feedback at: <a href="https://github.com/iluminar/goodwork/issues" class="pl-2 text-xl text-blue-light no-underline">Goodwork Issues</a>
+    </div>
+    <div class="pt-8">
+      © {{ (new Date()).getFullYear() }} <a href="https://github.com/iluminar/" class="text-xl text-blue-light no-underline">Iluminar</a>
+    </div>
+  </div>
+</div>  
+</template>
+
+<script>
+export default {
+  props: {
+    activeTab: {
+      required: true,
+      type: String,
+    }
+  },
+  data: () => ({
+    loading: false,
+    showUpdateModal: false,
+    version: 0.0,
+    lastUpdated: '',
+    message: '',
+    update_available: false
+  }),
+  created () {
+    this.version = document.querySelector('meta[name="app-version"]').content
+    this.lastUpdated = document.querySelector('meta[name="last-updated"]').content
+  },
+  methods: {
+    checkForUpdates () {
+      this.loading = true
+      axios.get('/admin/check-for-update')
+           .then((response) => {
+             this.loading = false
+             this.message = response.data.message
+             this.update_available = response.data.update_available
+             this.showUpdateModal = true
+           })
+           .catch((error) => {
+             this.loading = false
+             this.message = response.data.message
+             this.update_available = false
+             this.showUpdateModal = true
+           })
+    },
+    closeUpdateModal () {
+      this.loading = false
+      this.showUpdateModal = false
+      this.$nextTick(function () {
+        this.loading = false
+      })
+    },
+    updateProject () {}
+  }
+}
+</script>
