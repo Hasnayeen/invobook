b48ddee965576456b755500eb65f067988b76520
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-09-08 17:21:25 +0000

    Apply fixes from StyleCI

--- config/about.php
+++ config/about.php
@@ -1,6 +1,6 @@
 <?php
- return array (
+ return [
   'current_version' => 0.2,
-  'latest_version' => 0.2,
-  'last_updated' => 'Sep 8, 2018',
-) ;
+  'latest_version'  => 0.2,
+  'last_updated'    => 'Sep 8, 2018',
+];
