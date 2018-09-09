c700dafc40a42a357109a16c54cfb36b0065f0a2
Hasnayeen <searching.nehal@gmail.com>
2018-09-09 16:35:45 +0600

    Update software related configuration system

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL Description="This image is used to setup Goodwork application"
 
 RUN echo "cgi.fix_pathinfo=0;" > /usr/local/etc/php-fpm.d/php.ini
 
-RUN apt-get update && apt-get install zip unzip && docker-php-ext-install pdo_mysql
+RUN apt-get update && apt-get -y install zip unzip git && docker-php-ext-install pdo_mysql
 
 # Install Composer and make it available in the PATH
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/ --filename=composer
---
+++ app/Utilities/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('about')) {
+    /**
+     * Get or set software related values
+     *
+     * @param  string $key
+     * @param  string $value
+     * @return array
+     */
+    function about($key, $value = null)
+    {
+        $string = file_get_contents(storage_path() . '/app/config/about.json');
+        $config = json_decode($string, true);
+
+        if (is_null($value)) {
+            return $config[$key];
+        }
+        
+        $config[$key] = $value;
+        file_put_contents(storage_path() . '/app/config/about.json', json_encode($config));
+
+        return $config;
+    }
+}
