8ed91417c109a1a66d96b35233a64c855aa95736
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-09-18 01:54:23 +0600

    Fix - registration invite email send to already registered user #142 (#155)
    * Fix - registration invite email send to already registered user #142

--- resources/assets/js/pages/users/profile.js
+++ resources/assets/js/pages/users/profile.js
@@ -1,11 +1,12 @@
 import './../../bootstrap'
 import navbar from './../../components/partials/navbar.vue'
 import profile from './../../components/users/profile.vue'
+import notificationPopup from './../../components/partials/notificationPopup.vue'
 
 /* eslint-disable no-unused-vars */
 const app = new Vue({
   el: '#app',
   components: {
-    navbar, profile
+    navbar, profile, notificationPopup
   }
 })
