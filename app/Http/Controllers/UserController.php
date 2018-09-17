4fa148f4e5eccb239a7900eecf3b9a25fe6eb895
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-09-17 19:47:38 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
     public function sentInvitationToRegister(Request $request)
     {
         try {
-            if (!User::exists('email', $request->email)) {
+            if (! User::exists('email', $request->email)) {
                 Mail::to($request->email)
                     ->send(new SendInvitationToRegister());
 
