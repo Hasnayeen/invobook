7fd279a44d9a58316392709f500b33c1097b6394
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-09-19 22:09:22 +0600

    Add - welcome mail after successful registration #26 (#158)

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,24 +1,18 @@
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        @if ($company)
-            <h2>{{ Auth::user()->name }} invited you to join Goodwork ({{ $company }})</h2>
-        @else
-            <h2>{{ Auth::user()->name }} invited you to join Goodwork</h2>
-        @endif
+@component('mail::message')
+@if ($company)
+##  {{ Auth::user()->name }} invited you to join Goodwork ({{ $company }})
+@else
+##  {{ Auth::user()->name }} invited you to join Goodwork
+@endif
 
-        <div>
-            Hi {{ $name }},
-            <p><strong>Goodwork</strong> is a simple project management and collaboration tool to help everyone in {{ $company or 'a company'}} to stay organized and get work done.
-            It's simple and easy! To join click the link below:
-            <br/>
-            <br/>
-            {{ url('register/' . $token) }}
-            </p>
-        </div>
 
-    </body>
-</html>
+Hi {{ $name }},
+
+**Goodwork** is a simple project management and collaboration tool to help everyone in {{ $company or 'a company'}} to stay organized and get work done.
+It's simple and easy! To join click the link below:
+
+@component('mail::button', ['url' => url('register/' . $token)])
+Join Now
+@endcomponent
+
+@endcomponent
