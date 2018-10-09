7311529c4ad849a3053883e9111b0d3fcbc8ea7e
Peter Krupa <peter.krupa@protonmail.com>
2018-10-09 19:25:55 +0200

    [#44] Log new activity record when new user registers (#176)
    * Log new activity record when new user registers #44
    
    - Implemented event listener processing Registered event
    - Subscribed listener for Registered event in EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
+use App\Listeners\LogUserRegistration;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -13,8 +14,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
+        Registered::class => [
+            LogUserRegistration::class,
         ],
     ];
 
