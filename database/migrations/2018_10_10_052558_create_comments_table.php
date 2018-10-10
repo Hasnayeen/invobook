8049af2af727b471d4fcc12641a128e33d1ac65b
Peter Krupa <xkrupa12@gmail.com>
2018-10-10 10:11:07 +0200

    [#173] Adding new migration into project & reseting clover.xml

---
+++ database/migrations/2018_10_10_052558_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('discussion_id');
+            $table->unsignedInteger('user_id');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('discussion_id')->references('id')->on('discussions')->onDelete('cascade');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
