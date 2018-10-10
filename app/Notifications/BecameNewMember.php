6d82c97f0567ed8cfca8f8836e9821f36e9e166c
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-10-11 01:22:59 +0600

    Add ShouldQueue interface

--- app/Notifications/BecameNewMember.php
+++ app/Notifications/BecameNewMember.php
@@ -4,9 +4,10 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class BecameNewMember extends Notification
+class BecameNewMember extends Notification implements ShouldQueue
 {
     use Queueable;
 
