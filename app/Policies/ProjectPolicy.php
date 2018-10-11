924608d1ca97f8b84bc4db232d90bfbd2d814d30
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-10-11 13:48:50 +0600

    Fix style

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\Project;
-use Spatie\Permission\Models\Permission;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ProjectPolicy
