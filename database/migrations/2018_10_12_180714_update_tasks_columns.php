5e31c73a86f78c6a8922d58f65d89b5b432b29d4
nbrabant <brabantnicolas59@gmail.com>
2018-10-12 21:01:50 +0200

    fix : the task's status_id column should be nullable

--- database/migrations/2018_10_12_180714_update_tasks_columns.php
+++ database/migrations/2018_10_12_180714_update_tasks_columns.php
@@ -14,7 +14,7 @@ class UpdateTasksColumns extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->integer('status_id')->unsigned();
+            $table->integer('status_id')->unsigned()->nullable();
         });
     }
 
