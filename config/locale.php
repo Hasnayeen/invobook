ab1acde265476a3f3059d9914836676576593089
GijsGoudzwaard <GijsGoudzwaard@users.noreply.github.com>
2018-10-14 23:30:52 +0200

    Added dutch system translations (#230)

--- config/locale.php
+++ config/locale.php
@@ -14,6 +14,7 @@ return [
         'en'    => 'English',
         'es-ES' => 'Spanish (Spain)',
         'pt-BR' => 'Portuguese (Brazil)',
+        'nl-NL' => 'Dutch (Netherlands)',
     ],
 
     'route_to_file' => [
--- resources/lang/nl-NL/pagination.php
+++ resources/lang/nl-NL/pagination.php
@@ -12,6 +12,6 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next'     => 'Next &raquo;',
+    'previous' => '« Vorige',
+    'next'     => 'Volgende »',
 ];
