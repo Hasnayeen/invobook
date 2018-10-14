8aa5834dbdbd93b2a0f2d11d9222b8a65f5a562c
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2018-10-14 19:41:28 +0200

    Added Spanish language (#223)

---
+++ resources/lang/es-ES/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Proyectos',
+    'Teams'             => 'Equipos',
+    'Offices'           => 'Oficinas',
+    'Add a new project' => 'Agregar un nuevo proyecto',
+    'Add a new team'    => 'Agregr un nuevo equipo',
+    'Add a new office'  => 'Agregar una nueva oficina',
+];
