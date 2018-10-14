30221a59150a8733044f333654a478bdf4df6e84
Gijs G <gijs.goudzwaard@gmail.com>
2018-10-14 23:21:24 +0200

    Moved nl locales to nl-NL / Added nl locale to config/locales.php / Added user locales for dutch language

---
+++ resources/lang/nl-NL/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Profile'               => 'Profiel',
+    'Account'               => 'Account',
+    'Name'                  => 'Naam',
+    'Title'                 => 'Titel bij ' . config('app.company_name'),
+    'Short Bio'             => 'Korte biografie',
+    'Time Zone'             => 'Tijdzone',
+    'First Day of the Week' => 'Eerste dag van de week',
+    'Language'              => 'Taal',
+    'Change Your Avatar'    => 'Verander je avatar',
+    'Email'                 => 'Email',
+    'Current Password'      => 'Huidige wachtwoord',
+    'New Password'          => 'Nieuw wachtwoord',
+    'Confirm Password'      => 'Bevestig Password',
+    'Update'                => 'Bijwerken',
+    'Delete Account'        => 'Verwijder Account',
+    'Preferred Language'    => 'Voorkeurstaal',
+];
