9f30352270a733ee1ef2fa1f2e4c76c5b6c0bc6d
José Postiga <josepostiga@icloud.com>
2018-10-14 17:43:16 +0100

    Add support for user account details update (#216)
    * Add support for user account details update
    
    Signed-off-by: Jose Postiga <josepostiga@icloud.com>
    
    * Update migration down method to drop week_start column
    
    Signed-off-by: Jose Postiga <josepostiga@icloud.com>
    
    * Fixes style issues
    
    Signed-off-by: Jose Postiga <josepostiga@icloud.com>
    
    * Add doctrine/dbal dependency needed for when dropping columns from sqlite dbs.
    
    Signed-off-by: Jose Postiga <josepostiga@icloud.com>
    
    * Extracted user profile data management from UserAccountController to a dedicated UserProfileController
    
    Signed-off-by: Jose Postiga <josepostiga@icloud.com>

---
+++ app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateUserProfile;
+
+class UserProfileController extends Controller
+{
+    public function update(UpdateUserProfile $request)
+    {
+        $request->user()->update($request->all());
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Account profile are updated',
+        ]);
+    }
+}
