f33f9809946db43c768f8ca777b4f2e7cc0e2df8
Ahmad Rezk <rezk84@gmail.com>
2018-10-15 08:28:22 +0300

    localize language files to Arabic (#235)

---
+++ resources/lang/ar/project.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Create Task'             => 'إنشاء مهمة',
+    'Create New Post'         => 'إنشاء مشاركة',
+    'Currently in room'       => 'حاليا فى الغرفة',
+    'write your message here' => 'أكتب رسالتك هنا',
+];
---
+++ resources/lang/ar/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Profile'               => 'الملف التعريفي',
+    'Account'               => 'الحساب',
+    'Name'                  => 'الاسم',
+    'Title'                 => 'المسمى الوظيفي في' . config('app.company_name'),
+    'Short Bio'             => 'سيرة ذاتية قصيرة',
+    'Time Zone'             => 'منطقة زمنية',
+    'First Day of the Week' => 'أول يوم فى الأسبوع',
+    'Language'              => 'اللغة',
+    'Change Your Avatar'    => 'غير صورتك',
+    'Email'                 => 'البريد الإلكتروني',
+    'Current Password'      => 'كلمة المرور الحالية',
+    'New Password'          => 'كلمة المرور الجديدة',
+    'Confirm Password'      => 'تأكيد كلمة المرور',
+    'Update'                => 'تحديث',
+    'Delete Account'        => 'حذف الحساب',
+    'Preferred Language'    => 'اللغة المفضلة',
+];
