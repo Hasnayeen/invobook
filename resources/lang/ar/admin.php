919cfb4cabdac38cd18c913d1e5c8dc148fd6413
Ahmad Rezk <rezk84@gmail.com>
2018-10-15 08:28:22 +0300

    localize language files to Arabic (#235)

---
+++ resources/lang/ar/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'Users'                          => 'المستخدمين',
+    'Roles & Permissions'            => 'الأدوار والصلاحيات',
+    'Activities'                     => 'الأنشطة',
+    'About'                          => 'عنا',
+    'Roles'                          => 'الأدوار',
+    'Roles'                          => 'الأدوار',
+    'Permissions'                    => 'الصلاحيات',
+    'Invite New Member'              => 'دعوة عضو جديد',
+    'Add New Role'                   => 'أضف دور جديد',
+    'Cancel'                         => 'إلغاء',
+    'Assign'                         => 'تخصيص',
+    'Attach'                         => 'إرفاق',
+    'Filters'                        => 'فلاتر',
+    'Apply'                          => 'تطبيق',
+    'Load More'                      => 'المزيد',
+    'Check for Updates'              => 'بحث عن تحديثات',
+    'Filter by Activity'             => 'فلتر بالنشاط',
+    'Filter by User'                 => 'فلتر بالمستخدم',
+    'Filter by Date'                 => 'فلتر بالتاريخ',
+    'Last Updated'                   => 'آخر تحديث',
+    'Author'                         => 'الكاتب',
+    'Submit issue & feedback at'     => 'أرسل مشكلة & وأضف رد على',
+    'Don\'t belongs to any role yet' => 'لا تنضم إلى أي دور من قبل',
+    'Don\'t have any permission yet' => 'لا تحتوى على أي صلاحية من قبل',
+];
---
+++ resources/lang/ar/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'المشاريع',
+    'Teams'             => 'الفرق',
+    'Offices'           => 'المكاتب',
+    'Add a new project' => 'أضف مشروع جديد',
+    'Add a new team'    => 'أضف فريق جديد',
+    'Add a new office'  => 'أضف مكتب جديد',
+];
