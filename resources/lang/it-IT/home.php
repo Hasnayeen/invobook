926c3fb08c76fb605ccc495f56a0a118d47502d4
Alessandro <alessandro.digilio@hotmail.it>
2018-10-15 02:08:18 +0200

    added italian translation

---
+++ resources/lang/it-IT/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Progetti',
+    'Teams'             => 'Gruppi',
+    'Offices'           => 'Uffici',
+    'Add a new project' => 'Aggiungi un nuovo progetto',
+    'Add a new team'    => 'Aggiungi un nuovo gruppo',
+    'Add a new office'  => 'Aggiungi un nuovo Ufficio',
+];
