25e83f5cca78b841769443b0b1fe7239a142d186
Gustavo Karkow <karkowg@gmail.com>
2018-10-16 14:24:06 -0400

    Show resource name under tab icon

--- resources/assets/js/components/partials/tabMenu.vue
+++ resources/assets/js/components/partials/tabMenu.vue
@@ -3,26 +3,32 @@
       <div @click="activateThisTab('tasks')"
         :class="[(active === 'tasks') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-tasks text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Tasks' | localize }}</span>
       </div>
       <div @click="activateThisTab('discussions')"
         :class="[(active === 'discussions') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-clipboard-list text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Discussions' | localize }}</span>
       </div>
       <div @click="activateThisTab('messages')"
         :class="[(active === 'messages') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-comments text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Messages' | localize }}</span>
       </div>
       <div @click="activateThisTab('events')"
         :class="[(active === 'events') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-calendar-alt text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Events' | localize }}</span>
       </div>
       <div @click="activateThisTab('files')"
         :class="[(active === 'files') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-file-alt text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Files' | localize }}</span>
       </div>
       <div @click="activateThisTab('activities')"
         :class="[(active === 'activities') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <i class="fas fa-bolt text-2xl"></i>
+        <span class="block text-xs font-regular pt-2">{{ 'Activities' | localize }}</span>
       </div>
     </div>
 </template>
