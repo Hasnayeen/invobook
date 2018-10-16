a4d3e15dfa08d201d4927dca9206d67c19ef2efd
Alexander Morozov <webdevelopacc@gmail.com>
2018-10-16 21:09:35 +0700

    add russian localization files #253 (#260)

---
+++ resources/lang/ru-RU/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Проекты',
+    'Teams'             => 'Команды',
+    'Offices'           => 'Офисы',
+    'Add a new project' => 'Добавить новый проект',
+    'Add a new team'    => 'Добавить новую команду',
+    'Add a new office'  => 'Добавить новый офис',
+];
