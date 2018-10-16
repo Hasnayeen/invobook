ec9c18caa7a7e1fe97dda9b8583619e51081d67c
Norman Steger <me@normansteger.com>
2018-10-16 20:51:39 +0200

    add german translation (#269)
    * add german translation
    
    * updated changelog
    
    * add german to locale config

---
+++ resources/lang/de/project.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Create Task'             => 'Aufgabe erstellen',
+    'Create New Post'         => 'Neuen Beitrag erstellen',
+    'Currently in room'       => 'Momentan im Raum',
+    'write your message here' => 'schreibe deine Nachricht hier',
+];
