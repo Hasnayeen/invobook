1b536e4cb057291a9fe8fce9a3d6135fab74535c
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-10-17 23:38:09 +0600

    Update TokenFactory.php

--- database/factories/TokenFactory.php
+++ database/factories/TokenFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 use Spatie\Permission\Models\Role;
 
 $factory->define(Token::class, function (Faker $faker) {
-	$role = Role::first();
+    $role = Role::first();
     return [
         'token' => encrypt($role->id),
         'email' => $faker->safeEmail,
