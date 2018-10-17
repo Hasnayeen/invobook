c0fd358d57d8d8c94a932f10d60950f4d1170a77
oriolhub <33755729+oriolhub@users.noreply.github.com>
2018-10-17 15:12:00 +0200

    Fix some Spanish misspellings (#278)
    * Update home.php
    
    * Update admin.php
    
    * Update passwords.php
    
    * Update project.php
    
    * Update validation.php

--- resources/lang/es-ES/home.php
+++ resources/lang/es-ES/home.php
@@ -5,6 +5,6 @@ return [
     'Teams'             => 'Equipos',
     'Offices'           => 'Oficinas',
     'Add a new project' => 'Agregar un nuevo proyecto',
-    'Add a new team'    => 'Agregr un nuevo equipo',
+    'Add a new team'    => 'Agregar un nuevo equipo',
     'Add a new office'  => 'Agregar una nueva oficina',
 ];
