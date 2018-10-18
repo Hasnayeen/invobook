57c8f6a6b6a341d532dcd974309c828343a27f51
Tommi Finnilä <tommi.finnila@preludi.fi>
2018-10-18 10:58:18 +0300

    Tabmenu: Hide resource names on smaller viewports; fixes #289

--- resources/assets/js/components/partials/tabMenu.vue
+++ resources/assets/js/components/partials/tabMenu.vue
@@ -3,32 +3,32 @@
       <div @click="activateThisTab('tasks')"
         :class="[(active === 'tasks') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faTasks" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Tasks' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Tasks' | localize }}</span>
       </div>
       <div @click="activateThisTab('discussions')"
         :class="[(active === 'discussions') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faClipboardList" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Discussions' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Discussions' | localize }}</span>
       </div>
       <div @click="activateThisTab('messages')"
         :class="[(active === 'messages') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faComments" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Messages' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Messages' | localize }}</span>
       </div>
       <div @click="activateThisTab('events')"
         :class="[(active === 'events') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faCalendarAlt" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Events' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Events' | localize }}</span>
       </div>
       <div @click="activateThisTab('files')"
         :class="[(active === 'files') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faFileAlt" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Files' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Files' | localize }}</span>
       </div>
       <div @click="activateThisTab('activities')"
         :class="[(active === 'activities') ? 'text-teal-dark font-semibold border-teal border-b-2 pb-4 -mb-4' : 'cursor-pointer', 'text-center w-1/6']">
         <font-awesome-icon :icon="faBolt" class="text-2xl"></font-awesome-icon>
-        <span class="block text-xs font-regular pt-2">{{ 'Activities' | localize }}</span>
+        <span class="hidden md:block text-xs font-regular pt-2">{{ 'Activities' | localize }}</span>
       </div>
     </div>
 </template>
