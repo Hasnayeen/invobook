2e458c06d4d0fdc98ec8449afc8bc7ac432e8bef
Jimmie Hansson <jimmie@pantapasen.se>
2018-10-19 18:25:11 +0200

    ISSUE-304 add sv-SE (Swedish translation)

---
+++ resources/lang/sv-SE/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Projekt',
+    'Teams'             => 'Team',
+    'Offices'           => 'Kontor',
+    'Add a new project' => 'Lägg till nytt projekt',
+    'Add a new team'    => 'Lägg till nytt team',
+    'Add a new office'  => 'Lägg till nytt kontor',
+];
