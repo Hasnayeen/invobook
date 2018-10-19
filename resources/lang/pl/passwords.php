975dc87b7f00089f1a33ff17b27382b64a381300
Jacek Sawoszczuk <jacek.sawoszczuk@gmail.com>
2018-10-19 15:54:42 +0200

    added polish translation (#303)

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hasła muszą mieć przynajmniej sześć znaków i pasować do potrwierdzenia.',
+    'reset'    => 'Twoje hasło zostało zresetowane!',
+    'sent'     => 'Wysłaliśmy ci e-mail z odnośnikiem resetującym hasło!',
+    'token'    => 'Ten toke resetujący hasło jest niepoprawny.',
+    'user'     => 'Nie możemy znaleźć użytkownika z tym adresem e-mail.',
+];
