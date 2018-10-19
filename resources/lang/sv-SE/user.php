01bd4f25cb18df0e83ba65e7f56e644224d7922d
Jimmie Hansson <jimmiehansson1985@gmail.com>
2018-10-19 19:58:58 +0200

    Feature/issue 304 sv se language (#306)

---
+++ resources/lang/sv-SE/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Profile'               => 'Profil',
+    'Account'               => 'Konto',
+    'Name'                  => 'Namn',
+    'Title'                 => 'Titel ' . config('app.company_name'),
+    'Short Bio'             => 'Kort Biografi',
+    'Time Zone'             => 'Tidszon',
+    'First Day of the Week' => 'Första dagen i veckan',
+    'Language'              => 'Språk',
+    'Change Your Avatar'    => 'Ändra Avatar',
+    'Email'                 => 'Epost',
+    'Current Password'      => 'Aktuellt Lösenrod',
+    'New Password'          => 'Nytt Lösenord',
+    'Confirm Password'      => 'Bekräfta Lösenord',
+    'Update'                => 'Uppdatera',
+    'Delete Account'        => 'Ta bort Konto',
+    'Preferred Language'    => 'Mitt Språk',
+];
