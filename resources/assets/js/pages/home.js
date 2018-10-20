a092f3d59b9cae64d0ca09f49f795604e48b2988
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2018-10-20 22:17:16 +0200

    Remove v-focus vue directive
    It didn't work as expected

--- resources/assets/js/pages/home.js
+++ resources/assets/js/pages/home.js
@@ -2,12 +2,6 @@ import './../bootstrap'
 import componentMixin from './commonComponent'
 import home from './../components/home.vue'
 
-Vue.directive('focus', {
-  update: function (el) {
-    el.focus()
-  }
-})
-
 /* eslint-disable no-unused-vars */
 const app = new Vue({
   el: '#app',
