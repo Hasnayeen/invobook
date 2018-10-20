ac471438802327d25469d37e44dfffc4f919f630
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2018-10-20 21:23:30 +0200

    Add v-focus vue directive

--- resources/assets/js/pages/home.js
+++ resources/assets/js/pages/home.js
@@ -2,6 +2,12 @@ import './../bootstrap'
 import componentMixin from './commonComponent'
 import home from './../components/home.vue'
 
+Vue.directive('focus', {
+  update: function (el) {
+    el.focus()
+  }
+})
+
 /* eslint-disable no-unused-vars */
 const app = new Vue({
   el: '#app',
