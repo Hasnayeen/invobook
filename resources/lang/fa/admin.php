2edbc11de26fcf43cf97aeb275353837047389c8
Mohsen Yazdani <imyazdani@gmail.com>
2018-10-20 17:41:23 +0330

    Add farsi langguage (#325)

---
+++ resources/lang/fa/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'Users'                          => 'کاربران',
+    'Roles & Permissions'            => 'نقش ها و مجوزها',
+    'Activities'                     => 'فعالیت ها',
+    'About'                          => 'درباره',
+    'Roles'                          => 'نقش ها',
+    'Role'                           => 'نقش',
+    'Permissions'                    => 'مجوزها',
+    'Invite New Member'              => 'دعوت از عضو جدید',
+    'Add New Role'                   => 'اضافه کردن نقش جدید',
+    'Cancel'                         => 'انصراف',
+    'Assign'                         => 'تخصیص',
+    'Attach'                         => 'پیوست',
+    'Filters'                        => 'فیلترها',
+    'Apply'                          => 'اعمال',
+    'Load More'                      => 'بارگزاری بیشتر',
+    'Check for Updates'              => 'بررسی برای آپدیت ها',
+    'Filter by Activity'             => 'فیلتر بر اساس فعالیت',
+    'Filter by User'                 => 'فیلتر بر اساس کاربر',
+    'Filter by Date'                 => 'فیلتر بر اساس تاریخ',
+    'Last Updated'                   => 'آخرین بروزرسانی',
+    'Author'                         => 'نویسنده',
+    'Submit issue & feedback at'     => 'ثبت موضوع و بازخورد',
+    'Don\'t belongs to any role yet' => 'متعلق به هیچ نقشی نمی باشد',
+    'Don\'t have any permission yet' => 'دارای هیچ مجوزی نمی باشد',
+];
---
+++ resources/lang/fa/navbar.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Your Profile'  => 'پروفایل شما',
+    'Your Messages' => 'پیام های شما',
+    'Admin'         => 'ادمین',
+    'Logout'        => 'خروج',
+];
