c9a24c3c05647d3ccdb90b9b05bd7696167c1e33
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2018-10-21 21:57:50 +0200

    Croatian translation (#344)

---
+++ resources/lang/hr/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Lozinke moraju imati bar 6 znakova i slagati se s potvrdom lozinke.',
+    'reset'    => 'Tvoja lozinka je resetirana!',
+    'sent'     => 'e-mailom smo ti poslali link za resetiranje lozinke!',
+    'token'    => 'Ovaj token za resetiranje lozinke nije valjan.',
+    'user'     => 'Ne možemo pronaći korisnika s tom e-mail adresom.',
+];
