07cc538977a0cfa6b76c05cd265ac08370398470
Ian Mustafa <cuplizian@gmail.com>
2018-10-21 17:25:02 +0700

    Add Bahasa Indonesia language support (#337)

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Kata sandi harus terdiri dari minimal 6 karakter dan sesuai dengan konfirmasi kata sandi.',
+    'reset'    => 'Kata sandi Anda telah di-reset!', // TODO: get us better translation for "reset"
+    'sent'     => 'Kami telah mengirim tautan untuk me-reset kata sandi ke email Anda!',
+    'token'    => 'Token untuk me-reset kata sandi tidak valid.',
+    'user'     => 'Kami tidak dapat menemukan pengguna dengan email bersangkutan.',
+];
