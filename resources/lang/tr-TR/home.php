541c8e92eb997e3417da0db0c4d1f7439d93a799
Erman <ermanyilmz@gmail.com>
2018-10-22 09:37:10 +0300

    Tukish Translation to Home Page #299  (#346)

---
+++ resources/lang/tr-TR/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Projeler',
+    'Teams'             => 'Takımlar',
+    'Offices'           => 'Ofisler',
+    'Add a new project' => 'Yeni Proje Ekle',
+    'Add a new team'    => 'Yeni Takım Ekle',
+    'Add a new office'  => 'Yeni Ofis Ekle',
+];
