73c18f4248a12f1f890f9fe64a4d46494f5a2b58
Guilherme Pressutto <gpressutto5@gmail.com>
2018-10-24 01:11:48 -0300

    Added translation to pt-br

---
+++ resources/lang/pt-BR/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Profile'               => 'Perfil',
+    'Account'               => 'Conta',
+    'Name'                  => 'Nome',
+    'Title'                 => 'Título em ' . config('app.company_name'),
+    'Short Bio'             => 'Biografia pequena',
+    'Time Zone'             => 'Fuso horário',
+    'First Day of the Week' => 'Primeiro Dia da Semana',
+    'Language'              => 'Língua',
+    'Change Your Avatar'    => 'Mudar Sua Foto',
+    'Email'                 => 'Email',
+    'Current Password'      => 'Senha Atual',
+    'New Password'          => 'Nova Senha',
+    'Confirm Password'      => 'Confirmar Senha',
+    'Update'                => 'Atualizar',
+    'Delete Account'        => 'Apagar Conta',
+    'Preferred Language'    => 'Língua Preferida',
+];
