f74a96dc089505562935e37b1dce9f6158ded915
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2018-10-25 17:14:24 +0300

    added Hindi translation - home

---
+++ resources/lang/hi/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'परियोजनाओं',
+    'Teams'             => 'टीमें',
+    'Offices'           => 'कार्यालयों',
+    'Add a new project' => 'एक नई परियोजना जोड़ें',
+    'Add a new team'    => 'एक नई टीम जोड़ें',
+    'Add a new office'  => 'एक नया कार्यालय जोड़ें',
+];
