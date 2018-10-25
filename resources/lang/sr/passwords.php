ea20ef872bd89fc7c7598539231b00aaf9e20560
Aleksandar Savanovic <djaca85@gmail.com>
2018-10-25 20:34:55 +0200

    Add Serbian translation

---
+++ resources/lang/sr/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Lozinke moraju imati bar 6 znakova i slagati se sa potvrdom lozinke.',
+    'reset'    => 'Tvoja lozinka je resetovana!',
+    'sent'     => 'e-mailom smo ti poslali link za resetovanje lozinke!',
+    'token'    => 'Ovaj token za resetovanje lozinke nije validan.',
+    'user'     => 'Ne možemo pronaći korisnika sa tom e-mail adresom.',
+];
