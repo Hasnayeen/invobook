be985674f23a2b0d01e322ac97a2e37059474a11
Yuecheng Wu <ywu194@myseneca.ca>
2018-10-25 22:52:07 -0400

    add simplified Chinese translation

---
+++ resources/lang/zh-CN/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => '项目',
+    'Teams'             => '团队',
+    'Offices'           => '办公室',
+    'Add a new project' => '添加一个新的项目',
+    'Add a new team'    => '添加一个新的团队',
+    'Add a new office'  => '添加一个新的办公室',
+];
