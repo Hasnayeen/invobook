6dadd1f8ecf2022ab03b8ce06dc2700c3b57d2c3
Aleksandar Savanovic <djaca85@gmail.com>
2018-10-27 15:52:22 +0200

    Fix validation rules for task creation

--- app/Http/Requests/ValidateTaskCreation.php
+++ app/Http/Requests/ValidateTaskCreation.php
@@ -25,8 +25,8 @@ class ValidateTaskCreation extends FormRequest
     {
         return [
             'name'          => 'required|unique:tasks|max:255',
-            'assigned_to'   => 'nullable|exists:users,id',
-            'notes'         => 'string',
+            'assigned_to'   => 'required|exists:users,id',
+            'notes'         => 'nullable|string',
             'due_on'        => 'required|date_format:Y-m-d',
             'related_to'    => 'nullable|integer',
             'taskable_type' => 'required',
