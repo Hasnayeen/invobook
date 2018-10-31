7894f73604cc21d13c8982bd5d17049790454b96
redrabbittt <redrabbittt@redrabbitttui-MacBook-Pro.local>
2018-10-31 17:03:49 +0900

    added korean translation

---
+++ resources/lang/ko_KR/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '비밀번호는 최소 6자리 이상이어야 하며 비밀번호 확인란과 일치 해야 합니다.',
+    'reset'    => '비밀번호가 초기화 되었습니다!',
+    'sent'     => '비밀번호 초기화 링크를 이메일로 보내드렸습니다!',
+    'token'    => '비밀번호 초기화 토큰이 일치하지 않습니다.',
+    'user'     => '이메일과 일치하는 유저가 없습니다.',
+];
