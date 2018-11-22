c2fe7662a8ed39e97af0e3a8e48fb4aa27da4a3c
Mehedi Hassan <MehediDracula@gmail.com>
2018-11-22 21:51:58 +0600

    Fix styles

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -10,7 +10,7 @@ class TaskStatusController extends Controller
     public function update(UpdateTaskStatusRequest $request, Task $task)
     {
         $task->status()->updateOrCreate([], [
-            'name' => $request->name,
+            'name'  => $request->name,
             'color' => $request->color,
         ]);
 
