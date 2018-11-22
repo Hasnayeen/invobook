e4821506feb283e65f1c21166026b55629a8d715
Mehedi Hassan <MehediDracula@gmail.com>
2018-11-22 21:51:16 +0600

    Fix styles

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
-use Illuminate\Http\Request;
 use App\Http\Requests\UpdateTaskStatusRequest;
 
 class TaskStatusController extends Controller
