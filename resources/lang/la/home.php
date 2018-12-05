8d3ae646875df39fa509177b17cca0e3cfe77b79
PARAM MITTAL <parammittal16@gmail.com>
2018-12-05 15:16:50 +0530

    Add latin language translation, Fixes #332 (#437)

---
+++ resources/lang/la/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'projects',
+    'Teams'             => 'Teams',
+    'Offices'           => 'Offices',
+    'Add a new project' => 'addere novam project',
+    'Add a new team'    => 'equos addere novam',
+    'Add a new office'  => 'addere novum munus',
+];
---
+++ resources/lang/la/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Profile'               => 'Profile',
+    'Account'               => 'ratio',
+    'Name'                  => 'nomine',
+    'Title'                 => 'tituli ' . config('app.company_name'),
+    'Short Bio'             => 'Short Biography',
+    'Time Zone'             => 'Zona temporis',
+    'First Day of the Week' => 'Primo die ad Week',
+    'Language'              => 'Lingua',
+    'Change Your Avatar'    => 'Mutata tua Avatar',
+    'Email'                 => 'Email',
+    'Current Password'      => 'Codex Secretus Currens',
+    'New Password'          => 'novus absconditus verbem',
+    'Confirm Password'      => 'Constituo Password',
+    'Update'                => 'Update',
+    'Delete Account'        => 'Ratio delere',
+    'Preferred Language'    => 'msgstr',
+];
