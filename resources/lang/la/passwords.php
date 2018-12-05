255aa28b7c3164439a3ef5d73f13ad902804a094
rishabhscross <parammittal16@gmail.com>
2018-12-05 14:17:45 +0530

    Add latin language translation

---
+++ resources/lang/la/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Tesserae sex saltem par morum confirmatione.',
+    'reset'    => 'Reset vestri password est',
+    'sent'     => 'Reset vestri password link Index sumus e-CATAPHRACTUS!',
+    'token'    => 'Hoc reset password firmitatis indicium est irritum.',
+    'user'     => 'Non possumus invenire user cum E-mail.',
+];
