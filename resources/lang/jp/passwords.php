e0d9b59f0572747fae6c3acae9a942d3ab0e6363
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-06 23:35:58 +0530

    Add Japanese Localization #288

---
+++ resources/lang/jp/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードは6文字以上で、確認と一致していなければなりません。',
+    'reset'    => 'あなたのパスワードはリセットされました！',
+    'sent'     => 'パスワードリセットリンクが電子メールで送られてきました！',
+    'token'    => 'このパスワードリセットトークンは無効です。',
+    'user'     => 'そのメールアドレスを持つユーザーは見つかりません。',
+];
