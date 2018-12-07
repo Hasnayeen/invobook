973d207be3cc662d788304107cd946c75feb185c
PARAM MITTAL <parammittal16@gmail.com>
2018-12-07 15:34:31 +0530

    Add Danish language translation (#442)

---
+++ resources/lang/da/home.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Projects'          => 'Projekter',
+    'Teams'             => 'Hold',
+    'Offices'           => 'Kontorer',
+    'Add a new project' => 'Tilføj et nyt projekt',
+    'Add a new team'    => 'Tilføj et nyt hold',
+    'Add a new office'  => 'Tilføj et nyt kontor',
+];
