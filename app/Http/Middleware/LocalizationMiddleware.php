db657f6bd4bf22f10938d2989540b915aeb47739
Nehal Hasnayeen <searching.nehal@gmail.com>
2018-12-20 17:41:46 +0600

    Apply fixes from StyleCI (#452)

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -23,7 +23,7 @@ class LocalizationMiddleware
         $uri = $request->route() ? $request->route()->uri : '';
         $file = config('locale.route_to_file.' . $uri);
 
-        if (!is_null($file)) {
+        if (! is_null($file)) {
             $localeData = array_merge(Lang::get('navbar', [], $locale), Lang::get($file, [], $locale));
         } else {
             $localeData = Lang::get('navbar', [], $locale);
