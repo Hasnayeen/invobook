eeaed1719640c56f6e1e34a881cb0b83a3196201
Anuj Jaha <er.anujjaha@gmail.com>
2019-01-18 19:09:39 +0530

    misc

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -12,7 +12,7 @@ class NotificationController extends Controller
     public function index()
     {
         return view('admin.notification', [
-            'notifications' => auth()->user()->unreadNotifications
+            'notifications' => auth()->user()->unreadNotifications,
         ]);
     }
 
