ce5c10799f206fe55b40e3b50bcaaec05b07cf97
Nehal Hasnayeen <searching.nehal@gmail.com>
2019-05-25 12:27:38 +0000

    Apply fixes from StyleCI

--- app/Core/Http/Kernel.php
+++ app/Core/Http/Kernel.php
@@ -53,13 +53,13 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'admin'                   => \App\Core\Http\Middleware\RedirectIfNotAdmin::class,
-        'auth'                   => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic'             => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings'               => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'cache.headers'          => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can'                    => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest'                  => \App\Core\Http\Middleware\RedirectIfAuthenticated::class,
-        'signed'                 => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle'               => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'auth'                    => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth.basic'              => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings'                => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers'           => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can'                     => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'                   => \App\Core\Http\Middleware\RedirectIfAuthenticated::class,
+        'signed'                  => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle'                => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
