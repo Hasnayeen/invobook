c9c8cf695b9ca2df35653802c324674934831753
Nehal Hasnayeen <searching.nehal@gmail.com>
2019-07-08 19:22:01 +0600

    Apply fixes from StyleCI (#537)

--- database/factories/StepFactory.php
+++ database/factories/StepFactory.php
@@ -3,14 +3,14 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Core\Models\Step;
-use Faker\Generator as Faker;
 use App\Core\Models\Task;
+use Faker\Generator as Faker;
 
 $factory->define(Step::class, function (Faker $faker) {
     return [
         'description' => $faker->sentence(5),
         'unknown'     => $faker->boolean(),
         'done'        => false,
-        'task_id'     => factory(Task::class)->create()->id
+        'task_id'     => factory(Task::class)->create()->id,
     ];
 });
