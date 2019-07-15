1721f76bf9cf8b0d4f6ae609cfb2af44db5b7175
Hasnayeen <searching.nehal@gmail.com>
2019-07-15 22:59:19 +0600

    Show public group in the home page

--- app/Core/Http/Controllers/HomeController.php
+++ app/Core/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Core\Http\Controllers;
 
+use App\Core\Models\Team;
+use App\Core\Models\Office;
+use App\Core\Models\Project;
 use App\Core\Repositories\TaskRepository;
 
 class HomeController extends Controller
@@ -14,9 +17,15 @@ class HomeController extends Controller
     public function index(TaskRepository $repository)
     {
         $currentWork = $repository->userCurrentlyWorkingOn(auth()->user()->id);
-        $projects = auth()->user()->projects->load('members');
-        $teams = auth()->user()->teams->load('members');
-        $offices = auth()->user()->offices->load('members');
+        $projects = auth()->user()->projects->load('members')->concat(
+            Project::where('public', true)->with('members')->get()
+        )->unique();
+        $teams = auth()->user()->teams->load('members')->concat(
+            Team::where('public', true)->with('members')->get()
+        )->unique();
+        $offices = auth()->user()->offices->load('members')->concat(
+            Office::where('public', true)->with('members')->get()
+        )->unique();
 
         auth()->user()->setAppends(['unread_direct_messages']);
 
