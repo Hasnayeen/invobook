1746b5fb200e214f48002961ba6516e5ab548eb2
Milad Shahi <aznadesign@gmail.com>
2019-10-05 13:59:22 +0330

    Update Farsi (Persian) translate

--- resources/lang/fa/project.php
+++ resources/lang/fa/project.php
@@ -5,4 +5,7 @@ return [
     'Create New Post'         => 'ایجاد نوشته جدید',
     'Currently in room'       => 'هم اکنون در اتاق',
     'write your message here' => 'پیام خود را اینجا بنویسید',
+    'Upload Files'                  => 'بارگذاری پرونده‌ها',
+    'Project has been made public'  => 'پروژه به صورت عمومی در دسترس قرار گرفت',
+    'Project has been made private' => 'پروژه به صورت خصوصی در دسترس قرار گرفت',
 ];
