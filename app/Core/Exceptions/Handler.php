9fdc26726cee821bb4b867ed6d67f5bd72db7599
Nehal Hasnayeen <searching.nehal@gmail.com>
2019-10-06 15:24:16 +0600

    Apply fixes from StyleCI (#672)

--- app/Core/Exceptions/Handler.php
+++ app/Core/Exceptions/Handler.php
@@ -6,8 +6,8 @@ use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
