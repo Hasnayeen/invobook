a95e93e14753848a7dcd516f33fb92177a91e00f
Binal Gajjar <binal.gajjar7@gmail.com>
2019-10-18 13:38:58 +0530

    Fix #711[Update arabic translation] (#718)
    * Fix #711[Update arabic translation]
    
    * Update admin.php
    
    * Update misc.php

---
+++ resources/lang/ar/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'لا تتطابق أوراق الاعتماد هذه مع سجلاتنا.',
+    'throttle' => 'عدد كبير جدا من محاولات تسجيل الدخول. الرجاء المحاولة مره أخرى في :seconds ثانيه.',
+];
