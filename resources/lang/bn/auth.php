16be63e3da71a56ca22df3a242afa477dd4ac51d
Binal Gajjar <binal.gajjar7@gmail.com>
2019-10-18 14:53:44 +0530

    Fix #680 [Bangla Translations]

---
+++ resources/lang/bn/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'এই প্রমাণপত্রাদি আমাদের রেকর্ডের সাথে মেলে না.',
+    'throttle' => 'অনেক লগইন প্রচেষ্টা. অনুগ্রহ করে আবার চেষ্টা করুন :seconds সেকেন্ড.',
+];
