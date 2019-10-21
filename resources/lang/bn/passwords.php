37491420f59f1951d411e29f5f0154a038325a64
Binal Gajjar <binal.gajjar7@gmail.com>
2019-10-21 22:02:04 +0530

    Fix #680 [Bangla Translations] (#719)

---
+++ resources/lang/bn/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'পাসওয়ার্ড অন্তত ছয় অক্ষর হতে হবে এবং নিশ্চিতকরণ মেলে.',
+    'reset'    => 'আপনার পাসওয়ার্ডটি রিসেট করা হয়েছে!',
+    'sent'     => 'আমরা আপনার পাসওয়ার্ড রিসেট লিংক ই-মেইল করেছি!',
+    'token'    => 'এই পাসওয়ার্ড রিসেট টোকেন অকার্যকর.',
+    'user'     => 'আমরা ওই ই-মেইল ঠিকানা দিয়ে কোনো ব্যবহারকারী খুঁজে পাচ্ছি না ।',
+];
