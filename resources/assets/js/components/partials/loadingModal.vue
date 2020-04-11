9475f3e3f196a8ab62b5ea7713a26a7991b8e037
Hasnayeen <searching.nehal@gmail.com>
2020-04-11 19:14:28 +0600

    Update loading modal

--- resources/assets/js/components/partials/loadingModal.vue
+++ resources/assets/js/components/partials/loadingModal.vue
@@ -1,15 +1,12 @@
 <template>
 <div v-if="loading || localLoadingState">
-  <div :class="{'fixed': loading}" class="h-screen w-screen z-50 flex flex-row items-center justify-center">
+  <div class="fixed h-screen w-screen z-60 flex flex-row items-center justify-center">
     <div class="bg-white p-8 rounded-lg">
-      <div class="dots w-32 flex flex-row justify-between items-center">
-        <div class="w-8 h-8 bg-pink-500"></div>
-        <div class="w-8 h-8 bg-pink-500"></div>
-        <div class="w-8 h-8 bg-pink-500"></div>
+      <div class="circle w-32 h-32 flex flex-row justify-between items-center">
       </div>
     </div>
   </div>
-  <div class="h-screen w-screen fixed inset-0 bg-gray-900 opacity-25 z-10"></div>
+  <div class="h-screen w-screen fixed inset-0 bg-gray-900 opacity-25 z-50"></div>
 </div>
 </template>
 
@@ -30,25 +27,16 @@ export default {
 </script>
 
 <style>
-.dots div {
+.circle {
+  border: 3px solid hsla(212, 100%, 62%, 0.2);
+  border-top-color: #667eea;
   border-radius: 50%;
-  animation: fade 0.8s ease-in-out alternate infinite;
+  animation: spin 1s linear infinite;
 }
 
-.dots div:nth-of-type(1) {
-  animation-delay: -0.4s;
-}
-
-.dots div:nth-of-type(2) {
-  animation-delay: -0.2s;
-}
-
-@keyframes fade {
-  from {
-    opacity: 1;
-  }
+@keyframes spin {
   to {
-    opacity: 0;
+    transform: rotate(360deg);
   }
 }
 </style>
