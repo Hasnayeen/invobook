2b048b4cee4c0fc8efad5dfb9362559d275c78b9
Burak <ftzero@users.noreply.github.com>
2020-08-05 10:50:27 +0300

    Update home.php

--- resources/lang/tr-TR/home.php
+++ resources/lang/tr-TR/home.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
+    'Home'              => 'Ana Sayfa',
     'Projects'          => 'Projeler',
     'Teams'             => 'Takımlar',
     'Offices'           => 'Ofisler',
     'Add a new project' => 'Yeni Proje Ekle',
     'Add a new team'    => 'Yeni Takım Ekle',
     'Add a new office'  => 'Yeni Ofis Ekle',
+    'What\'s on your plate today'  => 'Bugün yapmanız gereken işler',
+    'There is currently no work "In Progress" assigned to you' => 'Şu anda size atanmış bir "Devam Ediyor" iş yok',
 ];
