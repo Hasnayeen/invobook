159e18285002e7540e820987664c74fe3bf2243f
Nehal Hasnayeen <Hasnayeen@users.noreply.github.com>
2020-09-29 23:23:25 +0000

    Apply fixes from StyleCI

--- app/Base/Http/Requests/AppSettingRequest.php
+++ app/Base/Http/Requests/AppSettingRequest.php
@@ -24,7 +24,7 @@ class AppSettingRequest extends FormRequest
     public function rules()
     {
         return [
-            'status' => 'required|boolean'
+            'status' => 'required|boolean',
         ];
     }
 }
